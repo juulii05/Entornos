606e3b47b3c27a0504f0a737d9794ead45e9be4a
Julia <juliarus0505@gmail.com>
2025-11-17 09:05:12 +0100

    Ejercicio4 de java

---
+++ Ejercicio4.java
@@ -0,0 +1,10 @@
+public class Ejercicio1{
+    public static void main(String[] args) {
+        int n,m;
+        n = 5;
+        m = 10;
+        int resultado = n - m;
+
+        System.out.println("El resultado de 5 - 10 es: " + resultado);
+    }
+}
